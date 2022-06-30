90ce44290338c741907ee40fb1ed19572d66962f
frede <65610875+dkfrede@users.noreply.github.com>
2022-06-30 23:36:40 +0200

    Update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,session
+from flask import Flask, render_template,request,session,redirect
 from flask_session import Session
 
 app = Flask(__name__, template_folder ='html')
@@ -15,7 +15,7 @@ Session(app)
 @app.route("/home")
 def home():
     if session.get("token") and session['token'] != None:
-        return "login: "+session['token']
+        return render_template("home.html", logged=True, token=session['token'])
     log("Visitor", "User with IP **"+request.remote_addr+"** loaded the home page!",True)
     return render_template("home.html")
 @app.route("/login")
@@ -26,26 +26,24 @@ def login():
 def upload():
     log("Visitor", "User with IP **"+request.remote_addr+"** loaded the upload page!",True)
     return render_template("upload.html")
+@app.route("/dashboard")
+def dashboard():
+    if session.get("token") and session['token'] != None:
+        return render_template("dashboard.html")
+    return redirect("/login")
 @app.route("/download")
 def download():
     log("Downloader", "User with IP **"+request.remote_addr+"** downloaded our resource!",True)
     return api.download()
-@app.route("/scripts")
-def scripts():
-
-    return render_template("scripts.html")
-@app.route("/whitelist")
-def whitelist():
-    return render_template("whitelist.html")
 @app.route("/account")
 def account():
-    return render_template("account.html")
+    return render_template("account.html", token=session['token'], ip=request.remote_addr, admin=False)
 @app.route("/logout")
 def logout():
     if session.get("token"):
         session["token"] = None
-    log("Visitor", "User with IP **"+request.remote_addr+"** loaded the logout page!",True)
-    return "logged out"
+    log("Visitor", "User with IP **"+request.remote_addr+"** logged out!",True)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
